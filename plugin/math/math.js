8dd89649c580a44d8097db01f51c3afbb6c35a21
Fernando Vizeu Santos <fvizeus@gmail.com>
2017-05-29 18:01:40 -0300

    reveal.js

--- plugin/math/math.js
+++ plugin/math/math.js
@@ -1,8 +1,13 @@
-
+/**
+ * A plugin which enables rendering of math equations inside
+ * of reveal.js slides. Essentially a thin wrapper for MathJax.
+ *
+ * @author Hakim El Hattab
+ */
 var RevealMath = window.RevealMath || (function(){
 
 	var options = Reveal.getConfig().math || {};
-	options.mathjax = options.mathjax || 'https://cdn.mathjax.org/mathjax/latest/MathJax.js';
+	options.mathjax = options.mathjax || 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js';
 	options.config = options.config || 'TeX-AMS_HTML-full';
 
 	loadScript( options.mathjax + '?config=' + options.config, function() {
@@ -16,11 +21,12 @@ var RevealMath = window.RevealMath || (function(){
 			skipStartupTypeset: true
 		});
 
-
+		// Typeset followed by an immediate reveal.js layout since
+		// the typesetting process could affect slide height
 		MathJax.Hub.Queue( [ 'Typeset', MathJax.Hub ] );
 		MathJax.Hub.Queue( Reveal.layout );
 
-	
+		// Reprocess equations in slides when they turn visible
 		Reveal.addEventListener( 'slidechanged', function( event ) {
 
 			MathJax.Hub.Queue( [ 'Typeset', MathJax.Hub, event.currentSlide ] );
@@ -36,7 +42,7 @@ var RevealMath = window.RevealMath || (function(){
 		script.type = 'text/javascript';
 		script.src = url;
 
-
+		// Wrapper for callback to make sure it only fires once
 		var finish = function() {
 			if( typeof callback === 'function' ) {
 				callback.call();
@@ -46,14 +52,14 @@ var RevealMath = window.RevealMath || (function(){
 
 		script.onload = finish;
 
-
+		// IE
 		script.onreadystatechange = function() {
 			if ( this.readyState === 'loaded' ) {
 				finish();
 			}
 		}
 
-
+		// Normal browsers
 		head.appendChild( script );
 
 	}
